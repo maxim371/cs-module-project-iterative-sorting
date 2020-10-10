fb65398b369e9bfc85733273c664f18602a9a191
maxim371 <kwaku.dr@gmail.com>
2020-10-10 15:37:50 -0400

    Searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,10 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    items = len(arr)
+    for item in range(0, items):
+        if arr[item] == target:
+            index = item
+            return index
     return -1   # not found
 
 
@@ -9,6 +12,17 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    low = 0
+    high = len(arr) - 1
 
+    while low <= high:
+        mid = (low + high) // 2
 
+        if arr[mid] == target:
+            return mid
+        else:
+            if arr[mid] < target:
+                low = mid + 1
+            if arr[mid] > target:
+                high = mid - 1
     return -1  # not found
